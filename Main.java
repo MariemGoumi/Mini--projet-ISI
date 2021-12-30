9f3f0fc9d2b03f158b921ccfb7cf8adec5398ecb
MariemGoumi <96753443+MariemGoumi@users.noreply.github.com>
2021-12-30 00:15:00 +0000

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-      System.out.println(" ######### DEBUT #############");
-      System.out.println(" Bonnes vacances !");
-      System.out.println(" ######### FIN #############");
-    }
-}
